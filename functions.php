1d043eea43219b2dd824b10f7da69c0e11bfacc8
Agus Suhanto <agus.suhanto@gmail.com>
2019-01-05 04:58:21 +0700

    Add sidebar to course post type editor

--- functions.php
+++ functions.php
@@ -1,4 +1,7 @@
 <?php
+/**
+ * Add main style.
+ */
 add_action( 'wp_enqueue_scripts', 'mts_sense_enqueue_styles' );
 function mts_sense_enqueue_styles() {
  
@@ -10,4 +13,30 @@ function mts_sense_enqueue_styles() {
         array( $parent_style ),
         wp_get_theme()->get('Version')
     );
+}
+
+/**
+ * Remove parent theme's metabox configuration.
+ */
+add_action( 'init', 'mts_remove_metabox_action' );
+function mts_remove_metabox_action(){
+	remove_action('add_meta_boxes', 'mts_add_sidebar_metabox');
+}
+
+/**
+ * Add a "Sidebar" selection metabox to post, page, and course.
+ */
+add_action('add_meta_boxes', 'mts_child_add_sidebar_metabox', 12);
+function mts_child_add_sidebar_metabox() {
+	$screens = array('post', 'page', 'course');
+	foreach ($screens as $screen) {
+		add_meta_box(
+			'mts_sidebar_metabox',          // id
+			__('Sidebar', 'ad-sense' ),	    // title
+			'mts_inner_sidebar_metabox',    // callback
+			$screen,                        // post_type
+			'side',	                        // context (normal, advanced, side)
+			'low'                           // priority (high, core, default, low)
+		);
+	}
 }
